2f14edf3b045f0beb24b5db5de8b41f6a76f3d29
myunsyeya <ggie1212@naver.com>
2019-05-26 12:48:31 +0900

    dd

--- node.c
+++ node.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <conio.h>
 #include <malloc.h>
 
+#define KI _getch()
 typedef struct node{
 	char Data[32];
 	struct node* p_next;
@@ -12,37 +14,41 @@ void addString(NODE** p);
 void printString(NODE* p);
 void freeString(NODE** p_head, NODE** p);
 
+void deletionString();
+
 int main() {
 	NODE* p_head = (NODE*)malloc(sizeof(NODE));
-	NODE* p = p_head;
-	addString(&p);
-	if(!strcmp(p_head->Data, "escape")) p_head->p_next->p_next = NULL;
-	printString(p_head);
+	NODE* p = p_head;                                                             // 선언부 
+	
+	while(1){
+		printf("1. 작성   2. 출력   3. 삭제   ESC. 끝\n\n");
+		switch (KI) {
+		case '1': system("cls"); while(KI != 27) addString(&p); break;
+		
+		printString(p_head);                                                      // 함수부 
+		}
+		system("cls"); 
+	}
 	freeString(&p_head, &p);
 	system("pause");
-	return 0;
+	return 0;                                                                     // 후처리부 
 }
 
 void addString(NODE** p){
-	char str[] = "escape";
-	while(1){
 	if (*p != NULL){
 		(*p)->p_next = (NODE*)malloc(sizeof(NODE));
-		printf("문자열을 입력해주세요.\n\n*나가시려면 escape를 입력해주세요\n\n==> "); 
+		printf("문자열을 입력해주세요.\n\n*나가시려면 esc를 눌러주세요\n\n==> "); 
 		scanf("%s", (*p)->Data);
-		if(!strcmp((*p)->Data, str)) return;
 		*p = (*p)->p_next;
 		(*p)->p_next = NULL;
 	}
 	else{
 		*p = (NODE*)malloc(sizeof(NODE));
 		(*p)->p_next = (NODE*)malloc(sizeof(NODE));
-		printf("문자열을 입력해주세요.\n 나가시려면 escape를 입력해주세요\n\n==> "); 
+		printf("문자열을 입력해주세요.\n\n*나가시려면 esc를 눌러주세요\n\n==> "); 
 		scanf("%s", (*p)->Data);
-		if(!strcmp((*p)->Data, str)) return;
 		*p = (*p)->p_next;
 		(*p)->p_next = NULL;
-	}
 	}
 }
 
